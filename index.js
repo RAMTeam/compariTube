93c0d58f00380ddc31cca691c01caff09954345b
robaElshazly <roba1818@gmail.com>
2021-05-06 12:06:25 +1000

    display one channel details

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ function displaySearchResult(searchList,targetUl){
     targetUl.textContent=""
     for(item of searchList){
         let li=document.createElement('li')
+        li.data=item.snippet.channelId
 
         let img=document.createElement('img')
         img.src=item.snippet.thumbnails.default.url
@@ -49,5 +50,41 @@ function displaySearchResult(searchList,targetUl){
         li.appendChild(img)
         li.appendChild(name)
         targetUl.appendChild(li)
+
+        li.addEventListener('click',function(e){getChannelData(li.data,targetUl)})
     }    
+}
+//function(data){
+   // console.log(data.items[0].statistics.viewCount)
+function getChannelData(channelId,targetUl){
+    fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${channelId}&key=${key}`)
+    .then(data=>data.json())
+    .then(data=>data.items[0])
+    .then(function(data){displayChannelDetails(data,targetUl)})
+}
+
+function displayChannelDetails(data,targetUl){
+    targetUl.textContent=''
+    let li=document.createElement('li')
+
+    let img=document.createElement('img')
+    img.src=data.snippet.thumbnails.default.url
+    li.appendChild(img)
+
+    let name=document.createElement('h3')
+    name.textContent=data.snippet.title
+    li.appendChild(name)
+
+    let vidCount=document.createElement('h3')
+    vidCount.textContent=data.statistics.videoCount
+    li.appendChild(vidCount)
+
+    let viewCount=document.createElement('h3')
+    viewCount.textContent=data.statistics.viewCount
+    li.appendChild(viewCount)
+
+    let subCount=document.createElement('h3')
+    subCount.textContent=data.statistics.subscriberCount
+    li.appendChild(subCount)
+    targetUl.appendChild(li)
 }

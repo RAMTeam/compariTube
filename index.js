cf1536c851b724e1d4f75067575deee59e02cef7
MrAjMann <anthonyjmann87@gmail.com>
2022-05-12 17:49:34 +1000

    Adjusted the styling better adjust to smaller screen sizes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let key=" AIzaSyD2hZ3vbnDeTmf6gG8n-jlIRv0tQqCyCtE"
-let firstSearchUl=document.querySelector("#first-search-ul")
-let secondSearchUl=document.querySelector("#second-search-ul")
-let CompareDiv=document.getElementById("compare-column")
-let CompareButton=document.getElementById("compare-button")
-let firstForm=document.getElementById("first-search-form")
-let secondForm= document.getElementById("second-search-form")
-let compareResult = document.getElementById("compare-result")
-secondSearchUl.dataset.itemChosen=false
-firstSearchUl.dataset.itemChosen=false
-let categoriesTags=compareResult.querySelectorAll('.category')
+let key = " AIzaSyD2hZ3vbnDeTmf6gG8n-jlIRv0tQqCyCtE";
+let firstSearchUl = document.querySelector("#first-search-ul");
+let secondSearchUl = document.querySelector("#second-search-ul");
+let CompareDiv = document.getElementById("compare-column");
+let CompareButton = document.getElementById("compare-button");
+let firstForm = document.getElementById("first-search-form");
+let secondForm = document.getElementById("second-search-form");
+let compareResult = document.getElementById("compare-result");
+secondSearchUl.dataset.itemChosen = false;
+firstSearchUl.dataset.itemChosen = false;
+let categoriesTags = compareResult.querySelectorAll(".category");
 
 //let channel1   //channel1 json
 //let channel2   //channel2 json
@@ -18,238 +18,239 @@ let categoriesTags=compareResult.querySelectorAll('.category')
 //     button.textContent="Compare"
 //     CompareDiv.
 // }
-function emptyCategoryTags(){
-    let warning=compareResult.querySelector(".warning")
-    warning.textContent=""
-    for(item of categoriesTags){
-        item.textContent=""
-    }
+function emptyCategoryTags() {
+  let warning = compareResult.querySelector(".warning");
+  warning.textContent = "";
+  for (item of categoriesTags) {
+    item.textContent = "";
+  }
 }
-CompareDiv.style.visibility = "hidden"
-firstForm.addEventListener("submit",function(e){getSearchResult(e,firstSearchUl)})
-secondForm.addEventListener("submit",function(e){getSearchResult(e,secondSearchUl)})
+CompareDiv.style.visibility = "hidden";
+firstForm.addEventListener("submit", function (e) {
+  getSearchResult(e, firstSearchUl);
+});
+secondForm.addEventListener("submit", function (e) {
+  getSearchResult(e, secondSearchUl);
+});
 // get a search list using the form keywords
-function getSearchResult(event,targetUl){
-   
-    targetUl.dataset.itemChosen = false
+function getSearchResult(event, targetUl) {
+  targetUl.dataset.itemChosen = false;
 
-    CompareDiv.style.visibility = "hidden"
-    emptyCategoryTags()
-    event.preventDefault(); 
-    searchBar=event.target.querySelector('input[type=text]')
-    if(searchBar.value!=''){
-     fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${searchBar.value}&key=${key}`)
-    .then(response=>response.json())
-    .then(function(data){
+  CompareDiv.style.visibility = "hidden";
+  emptyCategoryTags();
+  event.preventDefault();
+  searchBar = event.target.querySelector("input[type=text]");
+  if (searchBar.value != "") {
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${searchBar.value}&key=${key}&maxResults=3`
+    )
+      .then((response) => response.json())
+      .then(function (data) {
         //console.log(filterUniqeChannels(data.items))
-       return filterUniqeChannels(data.items)})
-    .then(function(data){displaySearchResult(data,targetUl)})
-}
-    else{
-        let warning=document.createElement('li')
-        warning.textContent='Please enter a search keyword'
-        targetUl.textContent=""
-        targetUl.appendChild(warning)
-    }
-}
-
-
-function filterUniqeChannels(searchList){
-    let unique={}
-    let filtered=searchList.filter(item=>!unique[item.snippet.title] && (unique[item.snippet.title] = true))
-    return filtered
+        return filterUniqeChannels(data.items);
+      })
+      .then(function (data) {
+        displaySearchResult(data, targetUl);
+      });
+  } else {
+    let warning = document.createElement("li");
+    warning.textContent = "Please enter a search keyword";
+    targetUl.textContent = "";
+    targetUl.appendChild(warning);
+  }
 }
 
-function displaySearchResult(searchList,targetUl){
-    //console.log(searchList)
-    let thumbnail = targetUl.parentNode.querySelector(".profile-thumbnail")
-    thumbnail.innerHTML = ""
-    targetUl.textContent = ""
+function filterUniqeChannels(searchList) {
+  let unique = {};
+  let filtered = searchList.filter(
+    (item) => !unique[item.snippet.title] && (unique[item.snippet.title] = true)
+  );
+  return filtered;
+}
 
-    CompareButton.style.display="block"
-    CompareDiv.style.visibility="hidden"
+function displaySearchResult(searchList, targetUl) {
+  //console.log(searchList)
+  let thumbnail = targetUl.parentNode.querySelector(".profile-thumbnail");
+  thumbnail.innerHTML = "";
+  targetUl.textContent = "";
 
-    // targetUl.parentNode.textContent= ""
+  CompareButton.style.display = "block";
+  CompareDiv.style.visibility = "hidden";
 
-    for(item of searchList){
-        let li = document.createElement('li')
-        li.classList.add("thumbnails-list")
-        li.data=item.snippet.channelId
-        
-        let img=document.createElement('img')
-        img.classList.add("profile-thumbnail")
-        img.src=item.snippet.thumbnails.default.url
-        
-        let name = document.createElement('h3')
-        name.classList.add("profiles-names")
-        name.textContent=item.snippet.channelTitle
+  // targetUl.parentNode.textContent= ""
 
-        li.appendChild(img)
-        li.appendChild(name)
-        targetUl.appendChild(li)
+  for (item of searchList) {
+    let li = document.createElement("li");
+    li.classList.add("thumbnails-list");
+    li.data = item.snippet.channelId;
 
-        li.addEventListener('click',function(e){getChannelData(li.data,targetUl)})
-    }    
+    let img = document.createElement("img");
+    img.classList.add("profile-thumbnail");
+    img.src = item.snippet.thumbnails.default.url;
+
+    let name = document.createElement("h3");
+    name.classList.add("profiles-names");
+    name.textContent = item.snippet.channelTitle;
+
+    li.appendChild(img);
+    li.appendChild(name);
+    targetUl.appendChild(li);
+
+    li.addEventListener("click", function (e) {
+      getChannelData(li.data, targetUl);
+    });
+  }
 }
 //function(data){
-   // console.log(data.items[0].statistics.viewCount)
-function getChannelData(channelId,targetUl){
-    targetUl.dataset.itemChosen=true
-    fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${channelId}&key=${key}`)
-    .then(data=>data.json())
-    .then(data=>data.items[0])
-    .then(function(data){
-        targetUl.dataset.channel=JSON.stringify(data)
-        console.log(targetUl.dataset.channel)
-        console.log(data)
-        displayChannelDetails(data,targetUl)})
+// console.log(data.items[0].statistics.viewCount)
+function getChannelData(channelId, targetUl) {
+  targetUl.dataset.itemChosen = true;
+  fetch(
+    `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${channelId}&key=${key}`
+  )
+    .then((data) => data.json())
+    .then((data) => data.items[0])
+    .then(function (data) {
+      targetUl.dataset.channel = JSON.stringify(data);
+      console.log(targetUl.dataset.channel);
+      console.log(data);
+      displayChannelDetails(data, targetUl);
+    });
 }
 
-function displayChannelDetails(data,targetUl){
-    targetUl.textContent=''
-    // let li=document.createElement('li')
-    let thumbnail = targetUl.parentNode.querySelector(".profile-thumbnail")
-   
-    //create the image element
-    let img=document.createElement('img')
-    img.src=data.snippet.thumbnails.default.url
-    thumbnail.appendChild(img)
+function displayChannelDetails(data, targetUl) {
+  targetUl.textContent = "";
+  // let li=document.createElement('li')
+  let thumbnail = targetUl.parentNode.querySelector(".profile-thumbnail");
 
-    //create the name headding
-    let name=document.createElement('li')
-    name.textContent=data.snippet.title
-    
-    targetUl.appendChild(name).classList.add("profile-heading")
+  //create the image element
+  let img = document.createElement("img");
+  img.src = data.snippet.thumbnails.default.url;
+  thumbnail.appendChild(img);
 
+  //create the name headding
+  let name = document.createElement("li");
+  name.textContent = data.snippet.title;
 
-    //create the video count heading 
-    let vidCount=document.createElement('li')
-    vidCount.innerHTML = `<span class="profile-heading">Video Counts: </span>${data.statistics.videoCount}`
-    //vidCount.classList.add("vidCount")
-    targetUl.appendChild(vidCount).classList.add("profile-item")
+  targetUl.appendChild(name).classList.add("profile-heading");
 
-    let viewCount=document.createElement('li')
-    viewCount.innerHTML=`<span class="profile-heading">View Count: </span>${data.statistics.viewCount}`
-    //vidCount.classList.add("viewCount")
-    targetUl.appendChild(viewCount).classList.add("profile-item")
+  //create the video count heading
+  let vidCount = document.createElement("li");
+  vidCount.innerHTML = `<span class="profile-heading">Video Counts: </span>${data.statistics.videoCount}`;
+  //vidCount.classList.add("vidCount")
+  targetUl.appendChild(vidCount).classList.add("profile-item");
 
-    let subCount=document.createElement('li')
-    if (data.statistics.subscriberCount){
+  let viewCount = document.createElement("li");
+  viewCount.innerHTML = `<span class="profile-heading">View Count: </span>${data.statistics.viewCount}`;
+  //vidCount.classList.add("viewCount")
+  targetUl.appendChild(viewCount).classList.add("profile-item");
+
+  let subCount = document.createElement("li");
+  if (data.statistics.subscriberCount) {
     //subCount.classList.add("subCount")
-    subCount.innerHTML=`<span class="profile-heading">Subscriber Count: </span>${data.statistics.subscriberCount}`
-    }
-    else{
-        subCount.innerHTML=`<span class="profile-heading">Sunscribers Count: </span>Hidden`
-        data=JSON.parse(targetUl.dataset.channel)
-        data.statistics.subscriberCount = 0
-        targetUl.dataset.channel=JSON.stringify(data)
-        
-        
-    }
-    
+    subCount.innerHTML = `<span class="profile-heading">Subscriber Count: </span>${data.statistics.subscriberCount}`;
+  } else {
+    subCount.innerHTML = `<span class="profile-heading">Sunscribers Count: </span>Hidden`;
+    data = JSON.parse(targetUl.dataset.channel);
+    data.statistics.subscriberCount = 0;
+    targetUl.dataset.channel = JSON.stringify(data);
+  }
 
-    targetUl.appendChild(subCount).classList.add("profile-item")
-    // targetUl.appendChild(li)
+  targetUl.appendChild(subCount).classList.add("profile-item");
+  // targetUl.appendChild(li)
 }
 
+CompareButton.addEventListener("click", getCompare);
 
-CompareButton.addEventListener('click',getCompare)
-
-function getCompare(){
-    
-    CompareDiv.style.visibility = "visible"
-    console.log(firstSearchUl.dataset.itemChosen)
-    console.log(secondSearchUl.dataset.itemChosen)
-    emptyCategoryTags()
-    if(firstSearchUl.dataset.itemChosen!="false" && secondSearchUl.dataset.itemChosen!="false") {
-        
-        displayCompare()
-    }  
-    else{
-        
-        let warning=compareResult.querySelector(".warning")
-        warning.textContent="Please, Choose two channels from both sides"
-        warning.classList.add("warning")
-    }
+function getCompare() {
+  CompareDiv.style.visibility = "visible";
+  console.log(firstSearchUl.dataset.itemChosen);
+  console.log(secondSearchUl.dataset.itemChosen);
+  emptyCategoryTags();
+  if (
+    firstSearchUl.dataset.itemChosen != "false" &&
+    secondSearchUl.dataset.itemChosen != "false"
+  ) {
+    displayCompare();
+  } else {
+    let warning = compareResult.querySelector(".warning");
+    warning.textContent = "Please, Choose two channels from both sides";
+    warning.classList.add("warning");
+  }
 }
 
-function displayCompare(){
-    CompareDiv.style.visibility = "visible"
-    CompareButton.style.display = "none"
-    //compareResult.textContent=""
-    console.log(typeof firstSearchUl.dataset.itemChosen)
-        let channel1=JSON.parse(firstSearchUl.dataset.channel)
-        let channel2=JSON.parse(secondSearchUl.dataset.channel)
-        console.log("heeeeey")
+function displayCompare() {
+  CompareDiv.style.visibility = "visible";
+  CompareButton.style.display = "none";
+  //compareResult.textContent=""
+  console.log(typeof firstSearchUl.dataset.itemChosen);
+  let channel1 = JSON.parse(firstSearchUl.dataset.channel);
+  let channel2 = JSON.parse(secondSearchUl.dataset.channel);
+  console.log("heeeeey");
 
-    
+  let header = document.createElement("h2");
+  header.textContent = "winning stats";
+  header.classList.add("winning-stats-heading");
+  //video count elements creation------------------------------
+  let videoLabel = document.createElement("h4");
+  videoLabel.classList.add("cat-item-title");
+  categoriesTags[0].appendChild(videoLabel);
 
-    let header=document.createElement('h2')
-    header.textContent="winning stats"
-    header.classList.add("winning-stats-heading")
-    //video count elements creation------------------------------
-    let videoLabel=document.createElement('h4')
-    videoLabel.classList.add("cat-item-title")
-    categoriesTags[0].appendChild(videoLabel)
-    
-    videoLabel.textContent="Video count"
+  videoLabel.textContent = "Video count";
 
-    let winnerName1=document.createElement('p')
-    categoriesTags[0].appendChild(winnerName1)
-    winnerName1.classList.add("cat-item-winner")
+  let winnerName1 = document.createElement("p");
+  categoriesTags[0].appendChild(winnerName1);
+  winnerName1.classList.add("cat-item-winner");
 
-    //view count elements creation------------------------------
-    let viewLabel=document.createElement('h4')
-    categoriesTags[1].appendChild(viewLabel)
-    viewLabel.textContent = "View count"
-    viewLabel.classList.add("cat-item-title")
+  //view count elements creation------------------------------
+  let viewLabel = document.createElement("h4");
+  categoriesTags[1].appendChild(viewLabel);
+  viewLabel.textContent = "View count";
+  viewLabel.classList.add("cat-item-title");
 
-    let winnerName2=document.createElement('p')
-    categoriesTags[1].appendChild(winnerName2)
-    winnerName2.classList.add("cat-item-winner")
+  let winnerName2 = document.createElement("p");
+  categoriesTags[1].appendChild(winnerName2);
+  winnerName2.classList.add("cat-item-winner");
 
-    //subscriber elements creation------------------------------
-    let subLabel=document.createElement('p')
-    subLabel.textContent = "Subscriber count"
-    categoriesTags[2].appendChild(subLabel)
-    subLabel.classList.add("cat-item-title")
+  //subscriber elements creation------------------------------
+  let subLabel = document.createElement("p");
+  subLabel.textContent = "Subscriber count";
+  categoriesTags[2].appendChild(subLabel);
+  subLabel.classList.add("cat-item-title");
 
-    let winnerName3=document.createElement('p')
-    categoriesTags[2].appendChild(winnerName3)
-    winnerName3.classList.add("cat-item-winner")
-    //let labels=[videoLabel,viewLabel,subLabel]
+  let winnerName3 = document.createElement("p");
+  categoriesTags[2].appendChild(winnerName3);
+  winnerName3.classList.add("cat-item-winner");
+  //let labels=[videoLabel,viewLabel,subLabel]
 
-    //video count comparison
-        if(channel1.statistics.videoCount > channel2.statistics.videoCount){
-            winnerName1.textContent=channel1.snippet.title
-        }
-        else if(channel1.statistics.videoCount < channel2.statistics.videoCount){
-            winnerName1.textContent=channel2.snippet.title 
-        }
-        else{
-            winnerName1.textContent="Equal"
-        }
+  //video count comparison
+  if (channel1.statistics.videoCount > channel2.statistics.videoCount) {
+    winnerName1.textContent = channel1.snippet.title;
+  } else if (channel1.statistics.videoCount < channel2.statistics.videoCount) {
+    winnerName1.textContent = channel2.snippet.title;
+  } else {
+    winnerName1.textContent = "Equal";
+  }
 
-    //view count comparison    
-    if(channel1.statistics.viewCount > channel2.statistics.viewCount){
-        winnerName2.textContent=channel1.snippet.title 
-    }
-    else if(channel1.statistics.viewCount < channel2.statistics.viewCount){
-        winnerName2.textContent=channel2.snippet.title 
-    }
-    else{
-        winnerName2.textContent="Equal"
-    }
+  //view count comparison
+  if (channel1.statistics.viewCount > channel2.statistics.viewCount) {
+    winnerName2.textContent = channel1.snippet.title;
+  } else if (channel1.statistics.viewCount < channel2.statistics.viewCount) {
+    winnerName2.textContent = channel2.snippet.title;
+  } else {
+    winnerName2.textContent = "Equal";
+  }
 
-    //sub count comparison    
-    if(channel1.statistics.subscriberCount > channel2.statistics.subscriberCount){
-        winnerName3.textContent=channel1.snippet.title 
-    }
-    else if(channel1.statistics.subscriberCount < channel2.statistics.subscriberCount){
-        winnerName3.textContent=channel2.snippet.title 
-    }
-    else{
-        winnerName3.textContent="Equal"
-    }
+  //sub count comparison
+  if (
+    channel1.statistics.subscriberCount > channel2.statistics.subscriberCount
+  ) {
+    winnerName3.textContent = channel1.snippet.title;
+  } else if (
+    channel1.statistics.subscriberCount < channel2.statistics.subscriberCount
+  ) {
+    winnerName3.textContent = channel2.snippet.title;
+  } else {
+    winnerName3.textContent = "Equal";
+  }
 }
-
